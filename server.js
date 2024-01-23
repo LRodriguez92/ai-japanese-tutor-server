4ccc4e72d9c79500bbe62b6cbff760cca5ce82f0
Leo <Leonardo.Rodriguez92@gmail.com>
2024-01-22 22:18:47 -0500

    Implemented Cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const OpenAI = require('openai');
+const cors = require('cors');
 require('dotenv').config();
 
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
